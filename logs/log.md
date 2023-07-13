21e39756ccf49fd0d357c59ef309b27ebd75d79f
LunexCoding <lunexcoding@vk.com>
2023-07-13 15:19:16 +0700

    Update ArticleParser

--- logs/log.md
+++
@@ -1,5 +0,0 @@
-```python
-[12-Apr-23 07:11] -> parser -> [DEBUG]: Parser run
-[12-Apr-23 07:11] -> parser -> [INFO]: Last page number: <16>
-[12-Apr-23 07:11] -> parser -> [DEBUG]: Parser completed
-[12-Apr-23 07:11] -> parser -> [DEBUG]: Data writing was successful
--- parsers/parserArticles.py
+++ parsers/parserArticles.py
@@ -1,22 +1,26 @@
 import string
 import json
 from pathlib import Path
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+
 from log import logger
+from selectorsConfig import selectorsConfig
 
 
-DATA_DIRECTORY = Path("../data")
+DATA_DIRECTORY = Path("data")
 FILE_SUFFIX = ".json"
 log = logger.getLogger(__name__)
 
 
-class ArtricleParser:
+class ArticleParser:
     def __init__(self, url=None):
         self._url = url
         self._companyArticles = []
         self._companyName = None
+        self._lastPage = None
         self._browser = webdriver.Chrome()
         self._browser.maximize_window()
 
@@ -27,18 +31,13 @@ class ArtricleParser:
         log.debug("Данные успешно записаны")
 
     def _getArticles(self):
-        return self._browser.find_elements(By.CLASS_NAME, "tm-articles-list__item")
+        return self._browser.find_elements(By.CLASS_NAME, selectorsConfig.blog["articles"])
 
     def _addAritcle(self, articleData):
         self._companyArticles.append(articleData)
 
     def __getDescriptionXPATH(self, article, articleID):
-        xpaths = [
-            f"/html[1]/body[1]/div[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/article[{articleID}]/div[1]/div[5]/div[1]/div[1]/div[1]",
-            f"/html[1]/body[1]/div[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/article[{articleID}]/div[1]/div[5]/div[2]/div[1]/div[1]",
-            f"/html[1]/body[1]/div[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/article[{articleID}]/div[1]/div[4]/div[1]/div[1]/div[1]",
-            f"/html[1]/body[1]/div[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/article[{articleID}]/div[1]/div[5]/div[1]/div[1]/div[1]",
-        ]
+        xpaths = selectorsConfig.getXpathByParameters("article", "description", articleID)
         for xpath in xpaths:
             try:
                 return article.find_element(By.XPATH, xpath).text
@@ -46,11 +45,11 @@ class ArtricleParser:
                 continue
 
     def _getInfoAboutArticle(self, article, articleID):
-        title = article.find_element(By.CLASS_NAME, "tm-title__link").find_element(By.TAG_NAME, "span").text
-        date = article.find_element(By.TAG_NAME, "time").get_attribute("title")
-        rating = article.find_element(By.XPATH, f"/html[1]/body[1]/div[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/article[{articleID}]/div[2]/div[1]/span[1]").text
+        title = article.find_element(By.CLASS_NAME, selectorsConfig.article["title"]).text
+        date = article.find_element(By.TAG_NAME, selectorsConfig.article["date"]).get_attribute("title")
+        rating = int(article.find_element(By.XPATH, selectorsConfig.getXpathByParameters("article", "rating", articleID)).text)
         description = self.__getDescriptionXPATH(article, articleID)
-        print(f"Статья: {title[50:]}")
+        log.debug(f"Статья <{articleID}/{len(self._companyArticles) + 1}>: {title[:50] + '...'}")
         return {
             "title": title,
             "description": description,
@@ -65,28 +64,31 @@ class ArtricleParser:
             log.debug(f"Получена ссылка на следующую страницу <{'/'.join(url)}>")
             return "/".join(url)
 
-    def start(self):
-        log.debug("Старт ArticleParser")
-        self._browser.get(self._url)
-        self._companyName = self._browser.find_element(By.CLASS_NAME, "tm-company-card__name").text
+    def _fingPagination(self):
         try:
             self._lastPage = int([el.text for el in self._browser.find_elements(By.CLASS_NAME, "tm-pagination__page")][-1])
         except:
             self._lastPage = 1
         log.info(f"Последняя страница с номером: <{self._lastPage}>")
-        for page in range(1, self._lastPage + 1):
+
+    def start(self):
+        log.debug("Старт ArticleParser")
+        self._browser.get(self._url)
+        self._companyName = self._browser.find_element(By.XPATH, selectorsConfig.article["companyCardInfo"]).find_element(By.TAG_NAME, "a").text
+        self._fingPagination()
+        for page in range(1, 2 + 1):
             log.debug(f"Переход на страницу с номером <{page}>")
-            try:
-                for articleID, article in enumerate(self._getArticles(), start=1):
-                    self._addAritcle(self._getInfoAboutArticle(article, articleID))
-                self._browser.find_element(By.XPATH, "//a[@id='pagination-next-page']").click()
-                self._url = self._generateNextPageUrl(page)
-                self._browser.get(self._url)
-            except Exception as e:
-                if page > self._lastPage:
-                    log.error(f"Страницы с номером <{page}> не существует!")
-                    self._browser.close()
-                    break
+            articles = self._getArticles()
+            for articleID, article in enumerate(articles, start=1):
+                self._addAritcle(self._getInfoAboutArticle(article, articleID))
+            if page > self._lastPage:
+                log.error(f"Страницы с номером <{page}> не существует!")
+                self._browser.close()
+                break
+            self._browser.find_element(By.XPATH, selectorsConfig.page["nextPage"]).click()
+            self._url = self._generateNextPageUrl(page)
+            self._browser.get(self._url)
+        log.debug(f"У компании {self._companyName}: {len(self._companyArticles)} статей")
         log.debug("Парсер завершил работу успешно!")
 
     @property
@@ -95,6 +97,6 @@ class ArtricleParser:
 
 
 if __name__ == "__main__":
-    parser = ArtricleParser("https://habr.com/ru/companies/evateam/articles/page1/")
+    parser = ArticleParser("https://habr.com/ru/companies/ruvds/articles/page1/")
     parser.start()
     parser._writeFileCompanyArticles()
